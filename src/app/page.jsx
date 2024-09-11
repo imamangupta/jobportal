22ac1a784fe8dc97bd20dbff02f1f297c7e7848b
harshit-exe <nikamharshit55@gmail.com>
2024-09-11 21:00:27 +0530

    Correct The Folder Structure
    Chnage file from js to jsx and also delete unwanted components

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import NavBar from '@/components/NavBar'
+import React from 'react'
+
+function page() {
+  return (
+
+    <div>
+      
+    <NavBar/>
+
+    </div>
+  )
+
+}
+export default page
