3568f6daed2343f35095bb0d4e90b68e34001043
Abhishek-Sharma <abhishek.webdev001@gmail.com>
2024-09-13 17:51:27 +0530

    Solved Error  --Invalid-DOM-Property

--- src/components/SearchCourses.jsx
+++ src/components/SearchCourses.jsx
@@ -8,7 +8,7 @@ const SearchCourses = () => {
             <h1 className='font-[700] text-zinc-800 text-[2rem]'>Search Courses</h1>
             <div className="w-[max-content] h-[max-content] flex items-center justify-center gap-4">
                 <div className="w-[max-content] h-[max-content] flex items-center justify-center gap-2 px-3 py-1 bg-slate-100 shadow-md">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                    <svg width="24" height="24" fill="currentColor">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                     </svg>
                     <input type="text" name="searchCourse" id="searchCourse" placeholder='Search for over 50+ courses' className='h-[7dvh] w-[30dvw]  rounded border-none outline-none bg-transparent text-[1rem] text-zinc-900 px-2 font-[500]' />
