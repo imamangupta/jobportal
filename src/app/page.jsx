c2b63400264926981e577d0d7a2a9d1f8af3311e
Abhishek-Sharma <abhishek.webdev001@gmail.com>
2024-09-13 16:24:51 +0530

    Footer Section Completed --Responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Home from '../components/Home'
 import SearchCourses from '@/components/SearchCourses'
 import PopularCourses from '@/components/PopularCourses'
 import FAQ from '@/components/FAQ'
+import Footer from '@/components/Footer'
 import React from 'react'
 
 function page() {
@@ -13,6 +14,7 @@ function page() {
       <SearchCourses />
       <PopularCourses />
       <FAQ />
+      <Footer />
 
     </div>
   )
