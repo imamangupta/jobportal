2a3a04363225c1e0420ec12ff4e4db5f162cdcef
Aman Gupta <141570525+imamangupta@users.noreply.github.com>
2024-09-20 23:47:58 +0530

    add chat here

---
+++ src/components/Dashboard/component/chatsPage.js
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId='e9bd279e-8b53-4507-94f6-db36a94ec2b9'
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
